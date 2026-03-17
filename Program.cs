748e83e7599157a74da077199238cf5fa55ae35c
graysonshelton <grayson.shelton@petrollive.com>
2026-03-17 18:17:36 -0500

    Synching CORS

--- Program.cs
+++ Program.cs
@@ -96,14 +96,14 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowUI", policy =>
     {
         policy.WithOrigins(
-                "http://localhost:5173",
-                "http://10.5.0.2:5173",
-                "http://104.131.179.92",
-                "https://jellyfish-app-mm2x3.ondigitalocean.app"
-            )
-            .AllowAnyHeader()
-            .AllowAnyMethod()
-            .AllowCredentials();
+            "http://localhost:5173",
+            "http://10.5.0.2:5173",
+            "http://104.131.179.92",
+            "https://jellyfish-app-mm2x3.ondigitalocean.app"
+        )
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowCredentials();
     });
 });
 
