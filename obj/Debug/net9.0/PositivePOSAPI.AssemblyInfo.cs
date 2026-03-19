002d56f50edc3e27a29f69e8247ff792652ca8c1
graysonshelton <grayson.shelton@petrollive.com>
2026-03-18 19:22:56 -0500

    committing

--- obj/Debug/net9.0/PositivePOSAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/PositivePOSAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PositivePOSAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e08c8580bed553990d419165669b5a9784f3acfb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33fe223587b8bf7cc6c8e12ad058caf87dcc20dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("PositivePOSAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PositivePOSAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
